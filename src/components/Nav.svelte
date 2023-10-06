3b13fcceb9cfbd2c87b29f848d04ec3261420dd2
GeneralDeveloper1 <145347420+GeneralDeveloper1@users.noreply.github.com>
2023-10-06 22:06:36 +0900

    ✨유저 인증까지 파이어베이스로 구현

---
+++ src/components/Nav.svelte
@@ -0,0 +1,55 @@
+<script>
+  export let location;
+
+  const moveToHome = () => {
+    window.location.hash = "/";
+  };
+
+  const moveToMyPage = () => {
+    window.location.hash = "/my";
+  };
+</script>
+
+<footer>
+  <div class="footer-block">
+    <button class="footer-icons" on:click={moveToHome}>
+      <div class="footer-icons__img">
+        {#if location === "home"}
+          <img src="assets/home.svg" alt="" />
+        {:else}
+          <img src="assets/home_outline.svg" alt="" />
+        {/if}
+      </div>
+      <div class="footer-icons__desc">홈</div>
+    </button>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/location.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">동네생활</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/doc.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">내 근처</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/chat.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">채팅</div>
+    </div>
+
+    <button class="footer-icons" on:click={moveToMyPage}>
+      <div class="footer-icons__img">
+        {#if location === "my"}
+          <img src="assets/user_solid.svg" alt="" />
+        {:else}
+          <img src="assets/user.svg" alt="" />
+        {/if}
+      </div>
+      <div class="footer-icons__desc">나의 당근</div>
+    </button>
+  </div>
+</footer>
